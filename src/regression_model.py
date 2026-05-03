be88e56918d31c12cabbb844c98587a76cbb4802
Sasiwut Chaiyadecha <66057952+naenumtou@users.noreply.github.com>
2026-05-03 18:39:38 +0700

    fix bug

--- src/regression_model.py
+++ src/regression_model.py
@@ -52,7 +52,7 @@ def _expand_sign(
     return (
         sign_group
         .merge(pd.DataFrame({mev_col: base, "NEW": cols}), on = mev_col)
-        .assign(mev_col = lambda x: x["NEW"])
+        .assign(mev = lambda x: x["NEW"])
         .drop(columns = "NEW")
     )
 
